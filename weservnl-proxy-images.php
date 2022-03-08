385951cd531b48df571130a586a948c947131c59
Dina <dina@veda.com.br>
2022-03-08 17:13:50 -0300

    update url only for certain extensions

--- weservnl-proxy-images.php
+++ weservnl-proxy-images.php
@@ -9,15 +9,15 @@
 
 if ($_SERVER['REMOTE_ADDR'] !== "127.0.0.1") {
     add_filter('wp_get_attachment_url', function ($url) {
-        if (file_exists($url)) {
-            return $url;
+        if (in_array(mb_substr($url, -3), ['jpg', 'png', 'gif'])) {
+            return str_replace(site_url(), 'https://images.weserv.nl/?output=webp&url='.site_url(), $url);
         }
-        return str_replace(site_url(), 'https://images.weserv.nl/?output=webp&url='.site_url(), $url);
+        return $url;
     });
 
     add_filter('wp_calculate_image_srcset', function ($sources) {
         foreach ($sources as &$source) {
-            if (!file_exists($source['url'])) {
+            if (in_array(mb_substr($source['url'], -3), ['jpg', 'png', 'gif'])) {
                 $source['url'] = str_replace(site_url(), 'https://images.weserv.nl/?output=webp&url='.site_url(),
                     $source['url']);
             }
